d44ba20a770af4560066c9b3bda3680f2e9185e5
Somadina <somadinao@icloud.com>
2024-07-24 13:33:22 +0100

    testing

--- util/email.js
+++ util/email.js
@@ -27,6 +27,7 @@ module.exports = class Email {
     return nodemailer.createTransport({
       host: process.env.EMAIL_HOST,
       port: process.env.EMAIL_PORT,
+      secure: false,
       tls: {
         ciphers: "SSLv3",
       },
